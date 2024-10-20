d03d4348569463b1c15e7266d48026f9f165acfd
Vishnu Prakash <65424078+v-ishnu@users.noreply.github.com>
2024-10-21 02:41:53 +0530

    Update app.js

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -2,27 +2,53 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
+const jwt =require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+const userOTPverification = require("./models/userOTPverification");
+require('dotenv').config();
+
 
 app.use(cors());
 app.use(express.json());
 
-const mongoUrl = "mongodb+srv://vishnuprakash572:HjN3M3n6xwelC3BX@auxify.caita.mongodb.net/?retryWrites=true&w=majority&appName=AuxiFy";
-
 //  Configure DB
-mongoose.connect(mongoUrl).then(() => {
+mongoose.connect(process.env.mongoURL).then(() => {
     console.log(`Database Connected: ✅ Successfully`);
 }).catch((e) => {
     console.log(e);
 });
 
 // Import User model
-require('./appUserDetails');
+require('./models/appUserDetails');
 const User = mongoose.model("UserInfo");
 
 // Basic health check route
 app.get('/', (req, res) => {
     res.send({ status: "Started" });
 });
+
+// Login API
+app.post("/login", async(req, res)=>{
+    const {email,password} =req.body;
+
+    const oldUser=await User.findOne({email:email});
+
+    if (!oldUser){
+        return res.send({data:"User doesn't exist"})
+    }
+
+    if (await bcrypt.compare(password,oldUser.password)){
+        const token=jwt.sign({email:oldUser.email}, process.env.JWT_SECRET_KEY);
+
+        if (res.status(201)){
+            return res.send({status:"ok", data:token})
+        } else {
+            return res.send({error:"error"});
+        }
+    }
+});
+
 
 // Register API
 app.post('/register', async (req, res) => {
@@ -47,6 +73,28 @@ app.post('/register', async (req, res) => {
     }
 });
 
-app.listen(3001, () => {
+
+// 
+
+
+
+// User Data
+app.post("/userdata", async (req, res) => {
+    const { token } = req.body;
+    try {
+      const user = jwt.verify(token, JWT_SECRET);
+      const useremail = user.email;
+  
+      User.findOne({ email: useremail }).then((data) => {
+        return res.send({ status: "Ok", data: data });
+      });
+    } catch (error) {
+      return res.send({ error: error });
+    }
+  });
+
+
+
+app.listen(38345,() => {
     console.log('App is Running');
 });
